44aeed199865e74adb4e10366a50d5cfe2dbd57a
Alexander Ellis <alecellis1985@hotmail.com>
2018-09-07 11:15:30 -0300

    More changes

--- php/contact.php
+++ php/contact.php
@@ -38,7 +38,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "mathiascasa@yahoo.com";
+$address = "skim@stkarch.com";
 
 
 // Configuration option.
